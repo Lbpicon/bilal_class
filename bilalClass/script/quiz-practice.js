942887cef8513ddf40c875211dc6aca7e99e3ead
lbpicon <lrn.picon@gmail.com>
2019-04-21 12:31:14 -0500

    push it real good

--- bilalClass/script/quiz-practice.js
+++ bilalClass/script/quiz-practice.js
@@ -1,54 +1,8 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-function buildQuiz() {}
-
-function showResults() {}
-
-// display quiz right away
-buildQuiz();
-
-//on submit, show results
-submitButton.addEventListener('click,'showResults);
-
-const myQuestions = [
-    {
-        question: "Who is the strongest?",
-
-        answer: {
-            a: "Superman"
-
-            b: "The Terminator"
-
-            c: "Waluigi, obviously"
-        },
-
-        correctAnswer: "c"
-    }
-];
-
-function buildQuiz() {
-    const output = [];
-
-    myQuestions.forEach (
-        (currentQuestion, questionNumber) => {
-        
-        const answers = [];
-
-        for(letter in currentQuestion.answers){
-            answers.push(
-                <label>
-                    <input type="radio" name="questions${questionNumber}" value="${letter}">
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                </label>
-            );
-
-        }
-
-        }
-    )
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
 
 
-}
+const avengers = ['Captain America', 'Iron Man', 'Thor', 'Hulk', 'Hawkeye', 'Black Widow']
+
+avengers.concat(['Hulk', 'Hawkeye', 'Black Widow']);
